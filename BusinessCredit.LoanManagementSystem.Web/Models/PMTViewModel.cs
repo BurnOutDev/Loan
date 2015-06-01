cdf0712a48b345ba1e40b75cbfa8a4eb46aff8a1
BurnOutDev <iraklimurusidze@live.com>
2015-06-01 10:26:29 +0400

    Commit_ChangeTaxOrderID_To_String

--- BusinessCredit.LoanManagementSystem.Web/Models/PMTViewModel.cs
+++ BusinessCredit.LoanManagementSystem.Web/Models/PMTViewModel.cs
@@ -8,7 +8,7 @@ namespace BusinessCredit.LoanManagementSystem.Web.Models
     public class PMTViewModel
     {
         public int LoanId { get; set; }
-        public int TaxOrderId { get; set; }
+        public string TaxOrderId { get; set; }
         public double CurrentPayment { get; set; }
         public DateTime PaymentDate { get; set; }
     }
